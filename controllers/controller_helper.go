174cb00c5ef7e2736e60dcebddd1379341885f40
Yuhui Shi <yuhuishi@umich.edu>
2020-07-27 21:10:11 -0700

    Guestbook controller logic

---
+++ controllers/controller_helper.go
@@ -0,0 +1,129 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	webappv1 "mykubecon/api/v1"
+	"net"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+)
+
+func (r *GuestBookReconciler) desiredDeployment(book webappv1.GuestBook, redis webappv1.Redis) (appsv1.Deployment, error) {
+	// create the deployment object of the guestbook app
+	// this is equivalent to writing a yaml file
+	depl := appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{APIVersion: appsv1.SchemeGroupVersion.String(), Kind: "Deployment"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      book.Name,
+			Namespace: book.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: book.Spec.Frontend.Replicas, // won't be nil because defaulting
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"guestbook": book.Name},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"guestbook": book.Name},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "frontend",
+							Image: "gcr.io/google-samples/gb-frontend:v4",
+							// NB(directxman12): sorry about these environment
+							// variable names -- they're what the official
+							// sample uses (since they used to be the official
+							// terms for Redis).  We should change them now that Redis
+							// has changed as well.
+							Env: []corev1.EnvVar{
+								{Name: "GET_HOSTS_FROM", Value: "env"},
+								{Name: "REDIS_MASTER_SERVICE_HOST", Value: redis.Status.LeaderService},
+								{Name: "REDIS_SLAVE_SERVICE_HOST", Value: redis.Status.FollowerService},
+							},
+							Ports: []corev1.ContainerPort{
+								{ContainerPort: 80, Name: "http", Protocol: "TCP"},
+							},
+							Resources: *book.Spec.Frontend.Resources.DeepCopy(),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// set the owner reference of the deployment to the guestbook object
+	if err := ctrl.SetControllerReference(&book, &depl, r.Scheme); err != nil {
+		return depl, err
+	}
+
+	return depl, nil
+}
+
+func (r *GuestBookReconciler) desiredService(book webappv1.GuestBook) (corev1.Service, error) {
+
+	// create the service object
+	svc := corev1.Service{
+		TypeMeta: metav1.TypeMeta{APIVersion: corev1.SchemeGroupVersion.String(), Kind: "Service"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      book.Name,
+			Namespace: book.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{Name: "http", Port: 8080, Protocol: "TCP", TargetPort: intstr.FromString("http")},
+			},
+			Selector: map[string]string{"guestbook": book.Name},
+			Type:     corev1.ServiceTypeLoadBalancer,
+		},
+	}
+
+	// always set the controller reference so that we know which object owns this.
+	if err := ctrl.SetControllerReference(&book, &svc, r.Scheme); err != nil {
+		return svc, err
+	}
+
+	return svc, nil
+}
+
+func urlForService(svc corev1.Service, port int32) string {
+	// notice that we unset this if it's not present -- we always want the
+	// state to reflect what we observe.
+	if len(svc.Status.LoadBalancer.Ingress) == 0 {
+		return ""
+	}
+
+	host := svc.Status.LoadBalancer.Ingress[0].Hostname
+	if host == "" {
+		host = svc.Status.LoadBalancer.Ingress[0].IP
+	}
+
+	return fmt.Sprintf("http://%s", net.JoinHostPort(host, fmt.Sprintf("%v", port)))
+}
+
+func (r *GuestBookReconciler) booksUsingRedis(obj handler.MapObject) []ctrl.Request {
+	listOptions := []client.ListOption{
+		// matching our index
+		client.MatchingField(".spec.redisName", obj.Meta.GetName()),
+		// in the right namespace
+		client.InNamespace(obj.Meta.GetNamespace()),
+	}
+	var list webappv1.GuestBookList
+	if err := r.List(context.Background(), &list, listOptions...); err != nil {
+		// TODO: we should log here!
+		return nil
+	}
+	res := make([]ctrl.Request, len(list.Items))
+	for i, book := range list.Items {
+		res[i].Name = book.Name
+		res[i].Namespace = book.Namespace
+	}
+	return res
+}
--- controllers/guestbook_controller.go
+++ controllers/guestbook_controller.go
@@ -20,9 +20,13 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	webappv1 "mykubecon/api/v1"
 )
@@ -37,17 +41,86 @@ type GuestBookReconciler struct {
 // +kubebuilder:rbac:groups=webapp.convect.ai,resources=guestbooks,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=webapp.convect.ai,resources=guestbooks/status,verbs=get;update;patch
 
-func (r *GuestBookReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("guestbook", req.NamespacedName)
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=list;watch;get;patch
+// +kubebuilder:rbac:groups=core,resources=services,verbs=list;watch;get;patch
 
-	// your logic here
+// Reconcile the guestbook request
+func (r *GuestBookReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	ctx := context.Background()
+	log := r.Log.WithValues("guestbook", req.NamespacedName)
+
+	log.Info("reconciling guestbook")
+
+	// get the guestbook object
+	var book webappv1.GuestBook
+	if err := r.Get(ctx, req.NamespacedName, &book); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// get the redis object
+	var redis webappv1.Redis
+	redisName := client.ObjectKey{Name: book.Spec.RedisName, Namespace: req.Namespace}
+	if err := r.Get(ctx, redisName, &redis); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// get the deployment of the redis
+	deployment, err := r.desiredDeployment(book, redis)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	// get the svc resource of the redis
+	svc, err := r.desiredService(book)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// what does this line do?
+	applyOpts := []client.PatchOption{client.ForceOwnership, client.FieldOwner("guestbook-controller")}
+
+	err = r.Patch(ctx, &deployment, client.Apply, applyOpts...)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.Patch(ctx, &svc, client.Apply, applyOpts...)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// set the status field
+	book.Status.URL = urlForService(svc, book.Spec.Frontend.ServingPort)
+
+	err = r.Status().Update(ctx, &book)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	log.Info("reconciled guestbook")
 
 	return ctrl.Result{}, nil
 }
 
+// SetupWithManager the manager with fields
 func (r *GuestBookReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	mgr.GetFieldIndexer().IndexField(
+		&webappv1.GuestBook{}, ".spec.redisName",
+		func(obj runtime.Object) []string {
+			redisName := obj.(*webappv1.GuestBook).Spec.RedisName
+			if redisName == "" {
+				return nil
+			}
+			return []string{redisName}
+		})
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&webappv1.GuestBook{}).
+		Owns(&corev1.Service{}).
+		Owns(&appsv1.Deployment{}).
+		Watches(
+			&source.Kind{Type: &webappv1.Redis{}},
+			&handler.EnqueueRequestsFromMapFunc{
+				ToRequests: handler.ToRequestsFunc(r.booksUsingRedis),
+			}).
 		Complete(r)
 }
